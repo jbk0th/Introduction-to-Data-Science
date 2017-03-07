ed48da9df5dff9e5b0f184069b20b836b095eaf9
Jason Koth <jbkoth51@gmail.com>
2017-03-07 13:08:09 -0800

    progress with Week1 modules

---
+++ Week1_Ex.py
@@ -0,0 +1,122 @@
+x = 1
+y = 2
+x + y
+
+def add_numbers(x,y,z = None, flag=False):
+	if (flag):
+		print('Flag is True')
+	if (z == None):
+		return x + y
+	else:
+		return x + y + z
+#print(add_numbers(1,3,flag=True))
+
+#assign a function to a variable
+a = add_numbers
+#print(a(1,2,flag=True))
+
+#Python Programming Language: Types and Sequences
+
+#Tuple has () instead of brackets, immutable
+x = (1,'a',3,'b') #tuples are immutbale, cannot be altereda
+#print(type(x))
+c = [1,'a',3, 'h']
+#lists are mutable so the values, and of it and the number of them can be changed
+#print(type(c))
+
+#append a list
+
+# c.append(3.3)
+# print(c)
+
+# for item in c:
+	# print(item)
+#i = 0 
+# while( i != len(c)):
+	# print(c[i])
+	# i = i + 1
+	
+#for strings variable[:] <- slice operator goes from start to just before end number
+#	
+# firstname = 'Jason Brian Koth'.split(' ')[0] #[0] selects the first element of the list
+# lastname = 'Jason Brian Koth'.split(' ')[-1] #[-1] selects the last element of the list
+# #focuses the list split on a specific character
+# print(firstname)
+# print(lastname)
+
+#must convert objects to strings before concatenating
+
+#dictionaries associate keys with values
+#Dictionary Representation
+x = {'Chris Brooks':'brooksch@umich.edu','Bill Gates':'billg@microsoft.com'}
+#Retrieve a value by indexing the keys
+x['Kevyn Collins-Thompson'] = None
+#itreate over all the keys
+# for name in x:
+	# print(x[name])
+
+#iterate over all the values in a list
+# for name, email in x.items():
+	# print(name)
+	# print(email)
+x = ('Christopher','Brooks','brooksch@umich.edu')
+# fname, lname, email = x	
+
+# sales_record = {
+# 'price' : 3.24,
+# 'num_items': 4,
+# 'person' :'Chris'}
+
+# sales_statement = '{} bought {} item(s) at a price of {} each for a total of {}'
+# print(sales_statement.format(sales_record['person'],sales_record['num_items'],sales_record['price'],sales_record['num_items']*sales_record['price']))
+
+########
+#Reading and Writing CSV Files
+########
+import csv
+
+ 
+with open('mpg.csv') as csvfile:
+	mpg = list(csv.DictReader(csvfile))
+#Each row becomes a dictionary, and if fieldnames is omitted the first row becomes the keys for all the subsequent entires in each dictionary, IMPT each row becomes a dictionary
+
+print(mpg[:3])
+
+mpg[0].keys()
+#keys() gives us the column names of our csv
+
+#find the average cty fuel economy across all carss. All values in the dictionaries are strings so we need to convert to float
+sum(float(d['cty']) for d in mpg) / len(mpg)) # uses d as an iterable variable across all dictionaries in the list, then it index that key and sums it for each cty mileage thats been pull from the mpg
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
