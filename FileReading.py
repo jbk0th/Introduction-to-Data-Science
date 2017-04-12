70e8d2f56631342a4cd3bc19a1084d9ae948f7c3
jbk0th <jbkoth51@gmail.com>
2017-04-12 12:57:29 -0700

    Started using Jupyter notebooks for Data Science assignments mmakes things much easier

---
+++ FileReading.py
@@ -0,0 +1,11 @@
+import os
+
+os.chdir('C:\\Users\\Jason\\Introduction-to-Data-Science')
+
+#changes directories into the absolute specified path !!! works !!!
+
+os.getcwd()
+
+#prints the current working directory
+
+print(os.path.abspath('Introduction-to-Data-Science/Energy Indicators.xls'))
