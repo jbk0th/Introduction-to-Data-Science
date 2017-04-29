59e85b709f3d6d1b920c85fd9c72c0f0bf62b5e2
jbk0th <jbkoth51@gmail.com>
2017-04-29 15:39:13 -0700

    Completed Week3 Assignment and 4 entries for Week4_Project, only run_ttest remaining

--- W3_1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import os
-
-os.chdir('C:\\Users\\Jason\\Introduction-to-Data-Science') #changes the directory of the script into the correct one to access the 'Energy Indicators file
-os.getcwd() #shows current working directory
-#df = pd.read_excel('C:\\Users\\Jason\\Introduction-to-Data-Science\\Energy Indicators.xls')
-	#the above method works as well for reading in the file with the full path name
-df = pd.read_excel('Energy Indicators.xls',header=1)
